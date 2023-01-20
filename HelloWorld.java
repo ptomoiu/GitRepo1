b9197ab5f46dc7eecbbadbd579c32be8a33aedfe
ptomoiu <119763719+ptomoiu@users.noreply.github.com>
2023-01-20 16:52:21 +0200

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World Aaaas122 !");
+        System.out.println("Hello, World Aaaas1223 !");
     }
 }
